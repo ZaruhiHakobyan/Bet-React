5a5b094343cf8afd595c10d9552cc2ef1681d1d0
ZaruhiHakobyan <h.zaruhi@hotmail.com>
2017-07-08 09:45:14 +0400

    Update Contact.js

--- my-app/src/Contact.js
+++ my-app/src/Contact.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class Contact extends Component {
+    constructor(name) {
+		super();
+		this.name = name;
+	}
     render() {
         return (
             <div className="Contact">
